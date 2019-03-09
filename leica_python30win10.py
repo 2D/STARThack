39e7a0e7f0e72aa7e950b653fc4b637e00d9a00c
Demin Dimitry <dmd1@bfh.ch>
2019-03-09 11:30:51 +0100

    python and jupyter simple scripts are controlling leica

--- leica_python30win10.py
+++ leica_python30win10.py
@@ -1,7 +1,7 @@
 import socket
 import os
 
-TCP_IP = '169.254.224.107'
+TCP_IP = '192.168.254.3'
 TCP_PORT = 1212
 buffer_size = 1024
 
@@ -10,10 +10,10 @@ s.settimeout(1)
 try:
    s.connect((TCP_IP, TCP_PORT))
    print('success')
-   command = '%R1Q,9027:0.52,0.73,0,0,0\r\n'
+   command = b'%R1Q,9027:0.52,0.73,0,0,0\r\n'
    print(command)
    s.send(command)
-   print(s.recv(4096))
+   #print(s.recv(4096))
    s.close()
 except socket.timeout as e:
    print(e)
