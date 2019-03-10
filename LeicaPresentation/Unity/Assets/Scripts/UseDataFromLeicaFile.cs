14284dc5e8c557c503831da0b71e7dda35b2964f
Antoine OMNES <antoine.omnes@utbm.fr>
2019-03-10 13:18:48 +0100

    add antoine folder

---
+++ LeicaPresentation/Unity/Assets/Scripts/UseDataFromLeicaFile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using UnityEngine;
+
+public class UseDataFromLeicaFile : MonoBehaviour
+{
+	[SerializeField]
+	private string jsonStringData;
+	[SerializeField]
+	private string _nameFileLeica = "data.json";
+	[SerializeField]
+	private DataOneMeasureLeicaAll datas;
+
+	private const double HEIGHT = 1.46;
+	
+	[SerializeField]
+	private GameObject prefabPointCoordinate;
+
+	private void Awake()
+	{
+		jsonStringData = File.ReadAllText(Application.dataPath + "/Ressources/" + _nameFileLeica);
+		
+		datas = JsonUtility.FromJson<DataOneMeasureLeicaAll>(jsonStringData);
+		
+	}
+
+	private void Start()
+	{
+		foreach (DataOneMeasureLeica dataFromMeasure in datas.datas)
+		{
+			if (dataFromMeasure.RC.Equals("0:1285")) continue;
+			GameObject gameObject = Instantiate(prefabPointCoordinate);
+			gameObject.name = dataFromMeasure.lengthSlope + ";" +
+							dataFromMeasure.angleHorizontal + ";" + dataFromMeasure.angleVertical;
+			gameObject.transform.position = convertSphereCoordinateInToCartesianCoordinates(float.Parse(dataFromMeasure.lengthSlope, CultureInfo.InvariantCulture.NumberFormat),
+				float.Parse(dataFromMeasure.angleHorizontal, CultureInfo.InvariantCulture.NumberFormat),
+				float.Parse(dataFromMeasure.angleVertical, CultureInfo.InvariantCulture.NumberFormat));
+		}
+	}
+
+	private Vector3 convertSphereCoordinateInToCartesianCoordinates(float r, float angleHorizontal, float angleVertical)
+	{
+		double x = r * Math.Sin(angleVertical) * Math.Cos(angleHorizontal);
+		double y = r * Math.Sin(angleVertical) * Math.Sin(angleHorizontal);
+		double z = r * Math.Cos(angleVertical) + HEIGHT;
+		
+		return new Vector3((float)x, (float)y, (float)z);
+	}
+}
+
+[Serializable]
+public class DataOneMeasureLeicaAll
+{
+	public List<DataOneMeasureLeica> datas;
+}
+
+[Serializable]
+public class DataOneMeasureLeica
+{
+	public string R1P;
+	public string angleHorizontal;
+	public string value0;
+	public string lengthSlope;
+	public string RC;
+	public string modeMeasure;
+	public string angleVertical;
+}
---
+++ LeicaPresentation/telnetLeicaComApp.py
@@ -0,0 +1,68 @@
+import os
+import socket
+import time
+import json
+import math
+
+TCP_IP = ‘169.254.224.107’
+TCP_PORT = 1212
+buffer_size = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(60)
+
+allData = []
+
+try:
+  s.connect((TCP_IP, TCP_PORT))
+  print(‘Connected\n’)
+  setTargetType = ‘%R1Q,17021:1\r\n’
+  s.send(setTargetType)
+  print(‘Set target type: ’ + s.recv(4096))
+  for l in range(1,13,1):
+     for i in range(30):
+        print(‘l =’ + str(l))
+        a = (i)*2*math.pi/30
+        #b = (l)*math.pi/180
+        moveCommand = ‘%R1Q,9027:’ + str(a) + ‘,’ + str(l*0.2) + ‘,0,0,0\r\n’
+        s.send(moveCommand)
+        print(str(i) + ' => Move: ' + s.recv(4096))
+        angleDistanceCommand = ‘%R1Q,17017:2\r\n’
+        # angleDistanceCommand = ‘%R1Q,2108:5000,2\r\n’
+        datas = None
+        for _ in range(3): # Attempt to get angle and distance this many times
+           s.send(angleDistanceCommand)
+           data = s.recv(4096)
+           datas = data.split(‘,’)
+           if datas[2] == ‘0:1285’: # Check for error ‘only angle measurement vaid’
+              print(‘No valid distance\n’)
+           else: # Otherwise no more attempts
+              break
+        datasDict = {}
+        datasDict[‘R1P’] = datas[0]
+        datasDict[‘value0’] = datas[1]
+        datasDict[‘RC’] = datas[2]
+        datasDict[‘angleHorizontal’] = datas[3]
+        datasDict[‘angleVertical’] = datas[4]
+        datasDict[‘lengthSlope’] = datas[5]
+        datasDict[‘modeMeasure’] = datas[6]
+
+        allData.append(datasDict)
+        print(len(datas))
+        print(datas)
+        for j in range(7):
+           print(datas[j])
+
+  s.close()
+
+  print(allData)
+  datas = {}
+  datas[‘datas’] = allData
+  #print(json.dumps(allData, indent=4, sort_keys=True))
+  with open(‘data.json’,‘w’) as outfile:
+     json.dump(datas,outfile)
+  #f=open(“save.txt”,“w+“)
+  #json.dump(allData, indent=4, sort_keys=True)
+
+except socket.timeout as e:
+  print(e)
